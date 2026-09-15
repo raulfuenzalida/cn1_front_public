4e3ad2fe04b46d4f7d5d3f3ad878d5ea9dddc702
BrazenGhost630 <fel.vargaso@duocuc.cl>
2026-09-15 20:39:02 -0300

    se restauró base path correcto para GitHub Pages de proyecto

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/cn1_front_public/',
   server: {
     port: 5174,
     proxy: {
