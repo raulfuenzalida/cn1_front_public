b8e4729ba462577ce59009235385313121dde9d2
BrazenGhost630 <fel.vargaso@duocuc.cl>
2026-09-15 20:35:34 -0300

    se corrigió base path para GitHub Pages de usuario

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cn1_front_public/',
+  base: '/',
   server: {
     port: 5174,
     proxy: {
